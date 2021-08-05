ff75d68e8bfb5e8a7999f4a29b649ce3e9bdf22e
Zeyull <13508085664@163.com>
2021-08-05 21:05:30 +0800

    Node事件循环/盒子居中/React Hook优势

--- practiceCode/practice-app/src/App.js
+++ practiceCode/practice-app/src/App.js
@@ -2,12 +2,10 @@ import React ,{ useEffect, useState } from 'react';
 import Practice  from './components/Practice';
 
 function App() {
-  const [number,setNumber] = useState(1);
 
   return (
     <div>
-      {/* <button onClick = {clickChangeNum}>onclick</button> */}
-
+      <Practice />
     </div>
   );
 }
--- practiceCode/practice-app/src/components/Practice/index.jsx
+++ practiceCode/practice-app/src/components/Practice/index.jsx
@@ -1,4 +1,21 @@
 import React,{useEffect,useState} from 'react'
 
 export default function Practice() {
+    
+    const [counter, setCounter] = useState(0);
+    const onAlertButtonClick = () => {
+        setTimeout(() => {
+          alert("Value"  + counter);
+        }, 3000);
+    };
+
+    return (
+        <div>
+        <p>You clicked {counter} times.</p>
+        <button onClick={() => setCounter(counter + 1)}>Click me</button>
+        <button onClick={onAlertButtonClick}>
+            Show me the value in 3 seconds
+            </button>
+        </div>
+    );
 }
